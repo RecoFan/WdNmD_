5d25b9ab7088344f2055f95d82f906dc32a9692e
XZMADAO <826021077@qq.com>
2019-10-18 14:36:07 +0800

    FallingBrick

---
+++ Assets/Sricpt/FallingBrick.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBrick : MonoBehaviour
+{
+    public float falling_time = 2f;
+    public float time_to_back = 3f;
+    public bool is_back;
+    public bool is_falling;
+    private Collider2D cd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cd = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (is_falling)
+        {
+            falling_time -= Time.deltaTime;
+            if (falling_time <= 0)
+            {
+                is_falling = false;
+                cd.enabled = false;
+                falling_time = 2f;
+                is_back = true;
+            }
+        }
+        if(is_back)
+        {
+            time_to_back -= Time.deltaTime;
+            if(time_to_back<=0)
+            {
+                cd.enabled = true;
+                time_to_back = 3f;
+                is_back = false;
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        is_falling = true;
+
+    }
+
+}
