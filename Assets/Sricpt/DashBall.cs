697fb7db9d4a617b116d40095600d8ac0226107e
XZMADAO <826021077@qq.com>
2019-10-22 00:48:30 +0800

    dashBall

--- Assets/Sricpt/DashBall.cs
+++ Assets/Sricpt/DashBall.cs
@@ -7,12 +7,14 @@ public class DashBall : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    Animator ani;
     public bool is_Touch;
     Movement move;
+    public ParticleSystem disappear;
+    public float recoverTime;
+    public float static_recoverTime = 5f;
     void Start()
     {
-        ani = GetComponent<Animator>();
+        recoverTime = static_recoverTime;
         move = FindObjectOfType<Movement>();
     }
 
@@ -20,14 +22,26 @@ public class DashBall : MonoBehaviour
     void Update()
     {
        // ani.SetBool("Is_Touch", true);
+       if(is_Touch)
+        {
+            recoverTime -= Time.deltaTime;
+            if(recoverTime <=0)
+            {
+                recoverTime = static_recoverTime;
+                is_Touch = false;
+                GetComponent<SpriteRenderer>().enabled = true;
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(!is_Touch)
         {
+            disappear.Play();
             Camera.main.transform.DOComplete();
             Camera.main.transform.DOShakePosition(.2f, .5f, 14, 90, false, true);
+            GetComponent<SpriteRenderer>().enabled = false;
             is_Touch = true;
             move.hasDashed = false;
         }
