65c06dfaac8daf828aea6a08e3937bc7d3b34472
XZMADAO <826021077@qq.com>
2019-10-23 17:39:40 +0800

    FallingStone

--- Assets/Sricpt/FallingBrick.cs
+++ Assets/Sricpt/FallingBrick.cs
@@ -4,29 +4,43 @@ using UnityEngine;
 
 public class FallingBrick : MonoBehaviour
 {
-    public float falling_time = 2f;
-    public float time_to_back = 3f;
+    public float falling_time ;
+    public float time_to_back;
+    public float static_falling_time = 2f;
+    public float static_time_to_back = 3f;
     public bool is_back;
-    public bool is_falling;
+    public bool is_rock;
     private Collider2D cd;
+    private Animator ani;
+    private SpriteRenderer sr;
+    public ParticleSystem falling_particle;
     // Start is called before the first frame update
     void Start()
     {
-        cd = GetComponent<Collider2D>();
+        cd = GetComponentInChildren<Collider2D>();
+        ani = GetComponent<Animator>();
+        sr = GetComponent < SpriteRenderer > ();
+        time_to_back = static_time_to_back;
+        falling_time = static_falling_time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (is_falling)
+        ani.SetBool("Is_Rock", is_rock);
+        if (is_rock)
         {
+            if(!falling_particle.isPlaying)
+            falling_particle.Play();
             falling_time -= Time.deltaTime;
             if (falling_time <= 0)
             {
-                is_falling = false;
+                is_rock = false;
                 cd.enabled = false;
-                falling_time = 2f;
+                sr.enabled = false;
+                falling_time = static_falling_time;
                 is_back = true;
+                falling_particle.Stop();
             }
         }
         if(is_back)
@@ -35,15 +49,12 @@ public class FallingBrick : MonoBehaviour
             if(time_to_back<=0)
             {
                 cd.enabled = true;
-                time_to_back = 3f;
+                sr.enabled = true;
+                time_to_back = static_time_to_back;
                 is_back = false;
             }
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        is_falling = true;
 
-    }
 
 }
---
+++ Assets/Sricpt/FallingStoneChild.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingStoneChild : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject father;
+    void Start()
+    {
+        father = transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        father.GetComponent<FallingBrick>().is_rock = true;
+
+
+    }
+}
