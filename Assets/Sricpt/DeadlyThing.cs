e0edeaf4756213cccc7f68f3e720616df94e3f67
XZMADAO <826021077@qq.com>
2019-11-07 17:26:44 +0800

    Level2_10

--- Assets/Sricpt/DeadlyThing.cs
+++ Assets/Sricpt/DeadlyThing.cs
@@ -4,22 +4,52 @@ using UnityEngine;
 
 public class DeadlyThing : MonoBehaviour
 {
-    float radian = 0;
-    Vector3 oldPos;
+
+
+    [Header("Moving")]
+    [Space]
     public float perRadian = 0.03f;
     public float radius = 0.08f;
+    float radian = 0;
+    Vector3 oldPos;
+
+    [Header("Circle")]
+    [Space]
+    public Transform aroundPoint;
+    public float angularSpeed;
+    public float aroundRadius;
+    private float angled;
+
+    [Header("Boolen")]
+    public bool isCircle;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(isCircle)
+        { 
+            transform.position = new Vector3(aroundPoint.position.x*-1*aroundRadius, aroundPoint.position.y, transform.position.z);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        oldPos = transform.position;
-        radian += perRadian;
-        float dy = Mathf.Cos(radian) * radius;
-        transform.position = oldPos + new Vector3(0, dy, 0);
+        if (!isCircle)
+        {
+            oldPos = transform.position;
+            radian += perRadian;
+            float dy = Mathf.Cos(radian) * radius;
+            transform.position = oldPos + new Vector3(0, dy, 0);
+        }
+        else
+        {
+            angled += (angularSpeed * Time.deltaTime) % 360;
+            float posX = aroundRadius * Mathf.Sin(angled * Mathf.Deg2Rad);
+            float posY = aroundRadius * Mathf.Cos(angled * Mathf.Deg2Rad);
+
+            transform.position = new Vector3(posX, posY, 0) + aroundPoint.position;
+
+        }
     }
 }
