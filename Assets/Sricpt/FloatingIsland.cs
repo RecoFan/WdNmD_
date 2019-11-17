44ed004c14e1bedb033b3a76de16201f2c48daef
XZMADAO <826021077@qq.com>
2019-11-17 13:40:50 +0800

    Level1

---
+++ Assets/Sricpt/FloatingIsland.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingIsland : MonoBehaviour
+{
+    public float perRadian = 0.03f;
+    public float radius = 0.08f;
+    float radian = 0;
+    Vector3 oldPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        oldPos = transform.position;
+        radian += perRadian;
+        float dy = Mathf.Cos(radian) * radius;
+        transform.position = oldPos + new Vector3(0, dy, 0);
+    }
+}
