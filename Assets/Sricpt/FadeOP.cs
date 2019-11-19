7b34d242a065bfeade7c4776280b32f2b657c68b
XZMADAO <826021077@qq.com>
2019-11-19 13:16:24 +0800

    OP

---
+++ Assets/Sricpt/FadeOP.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class FadeOP : MonoBehaviour
+{
+    public float fade_out;
+    public float fade_in;
+    public bool iscanvas;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if(!iscanvas)
+        GetComponent<SpriteRenderer>().DOFade(0, 0);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void fadein()
+    {
+        GetComponent<SpriteRenderer>().DOFade(1, fade_in);
+    }
+    void fadeout()
+    {
+        Debug.Log("1");
+        GetComponent<SpriteRenderer>().DOFade(0, fade_out);
+    }
+}
---
+++ Assets/Sricpt/OPmask.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class OPmask : MonoBehaviour
+{
+    public float biggertime;
+    public float smalltime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void bigmask()
+    {
+        transform.DOScale(new Vector3(50, 50, 1), biggertime);
+    }
+    void smallmask()
+    {
+        transform.DOScale(new Vector3(0, 0, 1), smalltime);
+    }
+}
